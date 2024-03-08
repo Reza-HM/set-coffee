e70a5435983fcabc10513285b6da5eef405f11e9
Reza Hamzei <hamzeiamir205@gmail.com>
2024-03-08 19:07:21 +0330

    added the footer section

---
+++ src/components/modules/footer/Article.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import styles from "./article.module.css";
+import { FC } from "react";
+
+type ArticleProps = {
+  title: string;
+  img: string;
+  comments: string;
+  date: string;
+  href: string;
+};
+
+const Article: FC<ArticleProps> = ({ title, img, comments, date, href }) => {
+  return (
+    <Link href={href} className={styles.article}>
+      <img width={75} height={65} src={img} alt="" />
+      <div>
+        <p className={styles.title}>{title}</p>
+        <div>
+          <p>{comments}</p>
+          <p dir="rtl">{date}</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default Article;
