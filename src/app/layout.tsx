624a6c9b41959b3a00ce25f09093e0857c4ae045
Reza Hamzei <hamzeiamir205@gmail.com>
2024-03-08 18:56:26 +0330

    added articles section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import AOSInit from "@/utils/aos";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "صفحه اصلی - SET Coffee | فروشگاه اینترنتی قهوه ست",
@@ -15,11 +16,15 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {" "}
+        <AOSInit />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
