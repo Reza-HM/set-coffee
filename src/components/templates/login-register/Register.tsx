e58d8da9dbb7222742cfaf4bfb38a1d6a35b00fe
Reza Hamzei <hamzeiamir205@gmail.com>
2024-03-09 12:12:26 +0330

    started the logic of login-register page

---
+++ src/components/templates/login-register/Register.tsx
@@ -0,0 +1,65 @@
+import { FC, useState } from "react";
+import styles from "./register.module.css";
+import Sms from "./Sms";
+
+type RegisterProps = {
+  showloginForm: () => void;
+};
+
+const Register: FC<RegisterProps> = ({ showloginForm }) => {
+  const [isRegisterWithPass, setIsRegisterWithPass] = useState(false);
+  const [isRegisterWithOtp, setIsRegisterWithOtp] = useState(false);
+
+  const hideOtpForm = () => setIsRegisterWithOtp(false);
+
+  return (
+    <>
+      {!isRegisterWithOtp ? (
+        <>
+          <div className={styles.form}>
+            <input className={styles.input} type="text" placeholder="نام" />
+            <input
+              className={styles.input}
+              type="text"
+              placeholder="شماره موبایل  "
+            />
+            <input
+              className={styles.input}
+              type="email"
+              placeholder="ایمیل (دلخواه)"
+            />
+            {isRegisterWithPass && (
+              <input
+                className={styles.input}
+                type="password"
+                placeholder="رمز عبور"
+              />
+            )}
+            <p
+              style={{ marginTop: "1rem" }}
+              className={styles.btn}
+              onClick={() => setIsRegisterWithOtp(true)}
+            >
+              ثبت نام با کد تایید
+            </p>
+            <button
+              style={{ marginTop: ".7rem" }}
+              className={styles.btn}
+              onClick={() => setIsRegisterWithPass(!isRegisterWithPass)}
+            >
+              ثبت نام با رمزعبور
+            </button>
+            <p className={styles.back_to_login} onClick={showloginForm}>
+              برگشت به ورود
+            </p>
+          </div>
+          <p className={styles.redirect_to_home}>لغو</p>
+        </>
+      ) : (
+        <Sms hideOtpForm={hideOtpForm} />
+      )}
+    </>
+  );
+};
+
+export default Register;
