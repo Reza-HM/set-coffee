8ffb0a72ef4839aa0f26f1138aa0f6c2c5093e28
Reza Hamzei <hamzeiamir205@gmail.com>
2024-03-11 11:32:01 +0330

    added refresh token generator function

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -10,23 +10,24 @@ const hashPassword = async (password: string): Promise<string> => {
   return hashedPassword;
 };
 
-const generateToken = (data: TokenData): string => {
-  const privateKey = process.env.privateKey || "";
+const generateAccessToken = (data: TokenData): string => {
+  const privateKey = process.env.AccessTokenPrivateKey || "";
   if (!privateKey) {
     throw new Error("Private key not provided");
   }
 
-  const token = jwt.sign({ ...data }, privateKey, { expiresIn: "24h" });
+  const token = jwt.sign({ ...data }, privateKey, { expiresIn: "60s" });
   return token;
 };
+
 const verifyPassword = async (password: string, hashedPassword: string) => {
   const isValid: boolean = await bcryptjs.compare(password, hashedPassword);
   return isValid;
 };
 
-const verifyToken = (token: string): TokenData | false => {
+const verifyAccessToken = (token: string): TokenData | false => {
   try {
-    const privateKey = process.env.privateKey || "";
+    const privateKey = process.env.AccessTokenPrivateKey || "";
     if (!privateKey) {
       throw new Error("Private key not provided");
     }
@@ -39,4 +40,20 @@ const verifyToken = (token: string): TokenData | false => {
   }
 };
 
-export { hashPassword, generateToken, verifyPassword, verifyToken };
+const generateRefreshToken = (data: TokenData): string => {
+  const privateKey = process.env.RefreshTokenPrivateKey || "";
+  if (!privateKey) {
+    throw new Error("Private key not provided");
+  }
+
+  const token = jwt.sign({ ...data }, privateKey, { expiresIn: "15d" });
+  return token;
+};
+
+export {
+  hashPassword,
+  generateAccessToken,
+  verifyPassword,
+  verifyAccessToken,
+  generateRefreshToken,
+};
