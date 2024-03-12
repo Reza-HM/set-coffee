6ed96a1399510410efe45d5d9885b09404e17ac9
Reza Hamzei <hamzeiamir205@gmail.com>
2024-03-12 12:23:27 +0330

    using the signin api in the DOM

--- src/components/templates/login-register/Login.tsx
+++ src/components/templates/login-register/Login.tsx
@@ -2,6 +2,8 @@ import React, { FC, useState } from "react";
 import styles from "./login.module.css";
 import Link from "next/link";
 import Sms from "./Sms";
+import { showSwal } from "@/utils/helpers";
+import { validateEmail, validatePassword } from "@/utils/auth";
 
 type LoginProps = {
   showRegisterForm: () => void;
@@ -9,8 +11,64 @@ type LoginProps = {
 
 const Login: FC<LoginProps> = ({ showRegisterForm }) => {
   const [isLoginWithOtp, setIsLoginWithOtp] = useState(false);
+  const [password, setPassword] = useState("");
+  const [phoneOrEmail, setPhoneOrEmail] = useState("");
 
   const hideOtpForm = () => setIsLoginWithOtp(false);
+
+  const loginWithPassword = async () => {
+    console.log(phoneOrEmail);
+    if (!phoneOrEmail) {
+      return showSwal("لطفا شماره تماس یا ایمیل را وارد کنید", "error", [
+        "چشم",
+        "بستن",
+      ]);
+    }
+
+    const isValidEmail = validateEmail(phoneOrEmail);
+    if (!isValidEmail) {
+      return showSwal("ایمیل وارد شده صحیح نیست", "error", [
+        "تلاش مجدد",
+        "بستن",
+      ]);
+    }
+
+    if (!password) {
+      return showSwal("پسورد را وارد کنید", "error", ["تلاش مجدد", "بستن"]);
+    }
+
+    const isValidPassword = validatePassword(password);
+    if (!isValidPassword) {
+      return showSwal("پسورد به اندازه کافی قوی نیست", "error", [
+        "تلاش مجدد",
+        "بستن",
+      ]);
+    }
+
+    const user = { email: phoneOrEmail, password };
+
+    const res = await fetch("/api/auth/signin", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
+    });
+    const data = await res.json();
+
+    console.log("Res ->", res);
+    if (res.status === 200) {
+      showSwal("با موفقیت لاگین شدین", "success", [
+        "ورود به پنل کاربری",
+        "بستن",
+      ]);
+    } else if (res.status === 422 || res.status === 401) {
+      showSwal("کاربری با این اطلاعات یافت نشد", "error", [
+        "تلاش مجدد",
+        "بستن",
+      ]);
+    } else if (res.status === 419) {
+      showSwal("ایمیل یا پسورد صحیح نیست", "error", ["تلاش مجدد", "بستن"]);
+    }
+  };
 
   return (
     <>
@@ -21,17 +79,23 @@ const Login: FC<LoginProps> = ({ showRegisterForm }) => {
               className={styles.input}
               type="text"
               placeholder="ایمیل/شماره موبایل"
+              value={phoneOrEmail}
+              onChange={(event) => setPhoneOrEmail(event.target.value)}
             />
             <input
               className={styles.input}
               type="password"
               placeholder="رمز عبور"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
             />
             <div className={styles.checkbox}>
               <input type="checkbox" name="" id="" />
               <p>مرا به یاد داشته باش</p>
             </div>
-            <button className={styles.btn}>ورود</button>
+            <button className={styles.btn} onClick={loginWithPassword}>
+              ورود
+            </button>
             <Link href={"/forget-password"} className={styles.forgot_pass}>
               رمز عبور را فراموش کرده اید؟
             </Link>
