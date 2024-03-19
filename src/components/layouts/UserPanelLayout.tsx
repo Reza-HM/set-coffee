340d2291f89d066d83d82ad36a7155e0169d93d7
Reza Hamzei <hamzeiamir205@gmail.com>
2024-03-19 13:40:57 +0330

    protected the user panel routes

--- src/components/layouts/UserPanelLayout.tsx
+++ src/components/layouts/UserPanelLayout.tsx
@@ -2,12 +2,20 @@ import React, { ReactNode } from "react";
 import styles from "./userPanelLayout.module.css";
 import Sidebar from "@/components/modules/p-user/Sidebar";
 import Topbar from "@/components/modules/p-user/Topbar";
+import authUser from "@/utils/getUserData";
+import { redirect } from "next/navigation";
 
 type LayoutProps = {
   children: ReactNode;
 };
 
-const Layout = ({ children }: LayoutProps) => {
+const Layout = async ({ children }: LayoutProps) => {
+  const user = await authUser();
+
+  if (!user) {
+    redirect("/login-register");
+  }
+
   return (
     <div className={styles.layout}>
       <section className={styles.section}>
