5577c0dbe542f77ab425f99d56752a4ed5601e34
Reza Hamzei <hamzeiamir205@gmail.com>
2024-03-21 13:32:18 +0330

    coded the models for ticket and departmends

---
+++ models/Ticket.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import DepartmentModel from "./Department";
+import SubDepartmentModel from "./SubDepartment";
+
+const schema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    department: {
+      type: mongoose.Types.ObjectId,
+      ref: "Department",
+      required: true,
+    },
+    subDepartment: {
+      type: mongoose.Types.ObjectId,
+      ref: "subDepartment",
+      required: true,
+    },
+    priority: {
+      type: Number,
+      default: 1,
+      enum: [1, 2, 3],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const model = mongoose.models.Ticket || mongoose.model("Ticket", schema);
+
+export default model;
