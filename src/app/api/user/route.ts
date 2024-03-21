6b77b0ce24639ff1de9b8c570ccf911eb8ec1f4d
Reza Hamzei <hamzeiamir205@gmail.com>
2024-03-21 13:12:02 +0330

    coded the user update api

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,33 @@
+import connectToDB from "@/configs/db";
+import UserModel from "../../../../models/User";
+import authUser from "@/utils/getUserData";
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    connectToDB();
+    const user = await authUser();
+    const body = await req.json();
+    const { name, email, phone } = body;
+
+    // Validation (You)
+
+    await UserModel.findOneAndUpdate(
+      { _id: user?._id },
+      {
+        $set: {
+          name,
+          email,
+          phone,
+        },
+      }
+    );
+
+    return Response.json(
+      { message: "User updated successfully :))" },
+      { status: 200 }
+    );
+  } catch (err) {
+    return Response.json({ message: err }, { status: 500 });
+  }
+}
