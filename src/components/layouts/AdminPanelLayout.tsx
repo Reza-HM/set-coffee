aab55aa84edfc04a3e1f747c17432f70e666ea39
Reza Hamzei <hamzeiamir205@gmail.com>
2024-03-26 10:40:01 +0330

    made user panel entrance conditionally

--- src/components/layouts/AdminPanelLayout.tsx
+++ src/components/layouts/AdminPanelLayout.tsx
@@ -2,8 +2,16 @@ import React, { ReactNode } from "react";
 import styles from "./adminPanelLayout.module.css";
 import Sidebar from "@/components/modules/p-admin/Sidebar";
 import Topbar from "@/components/modules/p-admin/Topbar";
+import authUser from "@/utils/getUserData";
+import { redirect } from "next/navigation";
 
-const Layout = ({ children }: { children: ReactNode }) => {
+const Layout = async ({ children }: { children: ReactNode }) => {
+  const user = await authUser();
+
+  if (user?.role !== "ADMIN") {
+    redirect("/");
+  }
+
   return (
     <div className={styles.layout}>
       <section className={styles.section}>
